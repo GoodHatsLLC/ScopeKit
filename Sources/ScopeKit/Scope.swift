f02dd705eedad4ac8593b96ce8725ea6053cea54
Adam Zethraeus <adam@zethrae.us>
2021-05-14 09:29:15 -0700

    order lifecycle callbacks around their external emissions

--- Sources/ScopeKit/Scope.swift
+++ Sources/ScopeKit/Scope.swift
@@ -35,6 +35,11 @@ private struct ScopeScan {
     let curr: Weak<ScopeBase>
 }
 
+private struct ActiveScan: Equatable {
+    let last: Bool
+    let curr: Bool
+}
+
 // MARK: Scope
 open class Scope: ScopeBase {
 
@@ -43,16 +48,16 @@ open class Scope: ScopeBase {
 
     let selfAllowsActiveSubject = CurrentValueSubject<Bool, Never>(false)
     var selfAllowsActivePublisher: AnyPublisher<Bool, Never> {
-        selfAllowsActiveSubject.eraseToAnyPublisher()
+        selfAllowsActiveSubject
+            .eraseToAnyPublisher()
     }
     let superscopeSubject = CurrentValueSubject<Weak<ScopeBase>, Never>(Weak(nil))
     var superscopePublisher: AnyPublisher<Weak<ScopeBase>, Never> {
         superscopeSubject
-            .removeDuplicates { $0.get() === $1.get() }
             .eraseToAnyPublisher()
     }
 
-    private var superIsActivePublisher: AnyPublisher<Bool?, Never> {
+    private var superIsActivePublisher: AnyPublisher<Bool, Never> {
         superscopePublisher
             .map { superscope in
                 superscope.get()?
@@ -63,13 +68,29 @@ open class Scope: ScopeBase {
                     .eraseToAnyPublisher()
             }
             .switchToLatest()
+            .replaceNil(with: false)
             .eraseToAnyPublisher()
     }
 
+    private let preExternalIsActiveScanSubject = PassthroughSubject<ActiveScan, Never>()
+    private let externalIsActiveSubject = CurrentValueSubject<Bool, Never>(false)
+    private let postExternalIsActiveScanSubject = PassthroughSubject<ActiveScan, Never>()
+
+    private var preExternalIsActivePublisher: AnyPublisher<Bool, Never> {
+        preExternalIsActiveScanSubject
+            .map { $0.curr }
+            .eraseToAnyPublisher()
+    }
+
+    // External
     override var isActivePublisher: AnyPublisher<Bool, Never> {
-        superIsActivePublisher
-            .replaceNil(with: false)
-            .combineLatest(selfAllowsActivePublisher) { $0 && $1 }
+        externalIsActiveSubject
+            .eraseToAnyPublisher()
+    }
+
+    private var postExternalIsActivePublisher: AnyPublisher<Bool, Never> {
+        postExternalIsActiveScanSubject
+            .map { $0.curr }
             .eraseToAnyPublisher()
     }
 
@@ -79,9 +100,16 @@ open class Scope: ScopeBase {
     }
 
     private func subscribeToLifecycle() {
-        isActivePublisher
-            .removeDuplicates()
-            .dropFirst(1) // Don't fire Stop for Initial value
+        preExternalIsActivePublisher
+            .sink(receiveValue: { [weak self] isActive in
+                guard let self = self else { return }
+                if isActive {
+                    self.willStart()
+                        .store(in: self.workBag)
+                }
+            }).store(in: lifecycleBag)
+
+        postExternalIsActivePublisher
             .sink(receiveCompletion: { [weak self] _ in
                 guard let self = self else { return }
                 self.willEnd()
@@ -89,17 +117,13 @@ open class Scope: ScopeBase {
                 self.lifecycleBag.cancel()
             }, receiveValue: { [weak self] isActive in
                 guard let self = self else { return }
-                if isActive {
-                    self.willStart()
-                        .store(in: self.workBag)
-                } else {
+                if !isActive {
                     self.willStop()
                     self.workBag.cancel()
                 }
             }).store(in: lifecycleBag)
 
         superscopePublisher
-            .dropFirst(1) // Don't fire for Initial value
             .removeDuplicates { $0.get() === $1.get() }
             .scan(ScopeScan(last: Weak(nil), curr: Weak(nil))) { prev, newScope in
                 ScopeScan(last: prev.curr, curr: newScope)
@@ -113,6 +137,27 @@ open class Scope: ScopeBase {
                     curr.retain(subscope: self)
                 }
             }.store(in: lifecycleBag)
+
+
+        // Order events to allow actions before and after subscopes
+        superIsActivePublisher
+            .combineLatest(selfAllowsActivePublisher) { $0 && $1 }
+            .scan(ActiveScan(last: false, curr: false)) { prev, newScope in
+                ActiveScan(last: prev.curr, curr: newScope)
+            }
+            // Only notify on a state switch
+            .filter { $0.last != $0.curr }
+            .sink(receiveCompletion: { [weak self] completion in
+                guard let self = self else { return }
+                self.preExternalIsActiveScanSubject.send(completion: completion)
+                self.externalIsActiveSubject.send(completion: completion)
+                self.postExternalIsActiveScanSubject.send(completion: completion)
+            }, receiveValue: { [weak self] value in
+                guard let self = self else { return }
+                self.preExternalIsActiveScanSubject.send(value)
+                self.externalIsActiveSubject.send(value.curr)
+                self.postExternalIsActiveScanSubject.send(value)
+            }).store(in: lifecycleBag)
     }
 
     public func enable() {
