52d7e1e1155617ccb5a8573d107d8feaab1de2de
Adam Zethraeus <adam@zethrae.us>
2021-06-04 08:02:09 -0700

    Revert "workers returns cancel bags not anycancancellable"
    This reverts commit 55e35baaa48bc6c16bef9d05f1659a784390c6b3.

--- Sources/ScopeKit/Worker.swift
+++ Sources/ScopeKit/Worker.swift
@@ -2,22 +2,22 @@ import Combine
 import Foundation
 
 public protocol WorkerType {
-    func start() -> CancelBag
+    func start() -> AnyCancellable
 }
 
 open class Worker: WorkerType {
 
     public init() {}
     /// Start the Worker's defined work.
-    public final func start() -> CancelBag {
+    public final func start() -> AnyCancellable {
         self.willStart()
     }
 
     /// Override this function to define work begun on `start()`.
     /// Do not call directly.
     /// `super.willStart()` call is not required.
-    open func willStart() -> CancelBag {
-        CancelBag()
+    open func willStart() -> AnyCancellable {
+        AnyCancellable({})
     }
 
 }
